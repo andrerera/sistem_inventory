7b81959ab6bdb5f50e4e138b7bcac8ece3a5902e
andrewan <andrewan653@gmail.com>
2025-05-21 22:09:23 +0700

    Edit Config.php to connection for neon db

--- app/config/config.php
+++ app/config/config.php
@@ -1,13 +1,15 @@
 <?php
-$host = 'localhost';
-$dbname = 'db_inventory';
-$user = 'postgres';
-$pass = 'andre123';
+$host = getenv('ep-mute-river-a5u5swcf-pooler.us-east-2.aws.neon.tech');
+$port = getenv('5432');
+$dbname = getenv('db_inventory');
+$username = getenv('andre_owner');
+$password = getenv('npg_8oqNJRxbuni9');
 
 try {
-    $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;sslmode=require", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Koneksi gagal: " . $e->getMessage());
+    error_log("Connection failed: " . $e->getMessage());
+    die("Connection failed: " . $e->getMessage());
 }
 ?>
---
+++ app/config/config_bck.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'db_inventory';
+$user = 'postgres';
+$pass = 'andre123';
+
+try {
+    $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Koneksi gagal: " . $e->getMessage());
+}
+?>
